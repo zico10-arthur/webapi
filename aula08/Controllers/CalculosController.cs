d76caba08a2288216b7a9bd10fcca23903a0a8a4
zico10-arthur <arthurrezendetere@gmail.com>
2025-10-17 17:31:46 -0300

    Adicionar arquivos de projeto.

---
+++ aula08/Controllers/CalculosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aula08.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculosController : ControllerBase
+    {
+        [HttpGet("Somar/{num1}/{num2}")]
+
+        public ActionResult Somar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return Ok($"A soma de {num1} + {num2} = {resultado}");
+        }
+
+        [HttpGet("divisao/{num1}/{num2}")]
+
+        public ActionResult dividir(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            return Ok($"A divisão de {num1} / {num2} = {resultado}");
+        }
+
+        [HttpGet("Subtrair/{num1}/{num2}")]
+
+        public ActionResult Subtração(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            return Ok($"A subttração de {num1} - {num2} = {resultado}");
+        }
+
+        [HttpGet("multiplicar/{num1}/{num2}")]
+
+        public ActionResult multiplicar(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            return Ok($"A multiplicação de {num1}* {num2} = {resultado}");
+        }
+    }
+}
